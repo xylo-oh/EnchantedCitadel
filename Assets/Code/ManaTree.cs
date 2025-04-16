dfb1ae4d22b546b51daa37c550b2a2637c211792
christopherthegoatlebron <christop.164953@nv.ccsd.net>
2025-04-16 09:04:21 -0700

    keep

--- Assets/Code/ManaTree.cs
+++ Assets/Code/ManaTree.cs
@@ -1,5 +1,21 @@
 using UnityEngine;
+public int maxCrystalsPerRound = 10;
 
+void SpawnManaCrystals()
+{
+    int spawnAmount = Mathf.Min(initialSpawnAmount + (currentRound * increasePerRound), maxCrystalsPerRound);
+
+    for (int i = 0; i < spawnAmount; i++)
+    {
+        Vector3 spawnPosition = Random.insideUnitSphere * spawnRadius;
+        spawnPosition.y = 0; 
+        spawnPosition += transform.position;
+
+        Instantiate(crystal, spawnPosition, Quaternion.identity);
+    }
+
+    currentRound++;
+}
 public class ManaTree : MonoBehaviour
 {
     public GameObject crystal;
@@ -7,30 +23,32 @@ public class ManaTree : MonoBehaviour
     public int initialSpawnAmount = 1;
     public int increasePerRound = 1;
     public float spawnRadius = 2f;
+    public int maxCrystalsPerRound = 10; 
     private int currentRound = 0;
 
     void Start()
     {
+        
         InvokeRepeating("SpawnManaCrystals", 0f, spawnInterval);
     }
 
     void SpawnManaCrystals()
     {
-        int spawnAmount = initialSpawnAmount + (currentRound * increasePerRound);
+        
+        int spawnAmount = Mathf.Min(initialSpawnAmount + (currentRound * increasePerRound), maxCrystalsPerRound);
 
         for (int i = 0; i < spawnAmount; i++)
         {
-
-            Vector3 spawnPosition = Random.insideUnitCircle * spawnRadius;
             
-           
-
-
+            Vector3 spawnPosition = Random.insideUnitSphere * spawnRadius;
+            spawnPosition.y = 0; 
             spawnPosition += transform.position;
 
+            
             Instantiate(crystal, spawnPosition, Quaternion.identity);
         }
 
+        
         currentRound++;
     }
 }
