5a3a23baa6f073f97e862db187b92dd48cd1cb93
lslvdr <jefferson.sosa74@gmail.com>
2025-04-23 10:44:01 -0700

    DONT WORRY

--- Assets/Code/Mutant.cs
+++ Assets/Code/Mutant.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EnemyCode : MonoBehaviour
+public class Mutant : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
