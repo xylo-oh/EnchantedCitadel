a9933886fcec4af54661ddeb142fb0bfa61a5b41
christopherthegoatlebron <christop.164953@nv.ccsd.net>
2025-04-29 09:15:09 -0700

    mutant healt

--- Assets/Code/Mutant.cs
+++ Assets/Code/Mutant.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Mutant : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int health = 10; // Enemy starts with 10 health
+
     void Start()
     {
-        
+        Debug.Log("Mutant spawned with health: " + health);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        // Check if the colliding object has the tag "Weapon"
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            TakeDamage(2); // Sword does 2 damage
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage; // Reduce health by the damage amount
+        Debug.Log("Mutant took damage! Current health: " + health);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Mutant has died!");
+        Destroy(gameObject); // Destroy the enemy GameObject
     }
 }
